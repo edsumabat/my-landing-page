744b56dde65c5cb486fa795ef73fe4212dcaebed
Edreinne <edreinnesumabat@gmail.com>
2025-02-06 16:34:28 +0800

    Changed colors of Homepage

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1$f2uw2o$3^$wz1l$@nq+#exx*c49150!mc3k$+d&5ow1h*(ll'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
